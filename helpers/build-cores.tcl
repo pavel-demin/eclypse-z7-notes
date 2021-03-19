3a14313aaf27cd49ae112322360a75c12b769a4f
Pavel Demin <pavel-demin@outlook.com>
2021-03-19 15:26:56 +0000

    add axis_ram_writer

--- helpers/build-cores.tcl
+++ helpers/build-cores.tcl
@@ -1,7 +1,7 @@
 set cores [list \
   axi_axis_reader_v1_0 axi_axis_writer_v1_0 axi_cfg_register_v1_0 \
-  axis_constant_v1_0 axis_fifo_v1_0 axis_lfsr_v1_0 axis_spi_v1_0 \
-  axi_sts_register_v1_0 axis_variable_v1_0 axis_zmod_adc_v1_0 \
+  axis_constant_v1_0 axis_fifo_v1_0 axis_lfsr_v1_0 axis_ram_writer_v1_0 \
+  axis_spi_v1_0 axi_sts_register_v1_0 axis_variable_v1_0 axis_zmod_adc_v1_0 \
   axis_zmod_dac_v1_0 dna_reader_v1_0 port_selector_v1_0 port_slicer_v1_0 \
 ]
 

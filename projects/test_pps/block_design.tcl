23d6b1f6d4fc2ca76f53b7c3e8cc5973ff23cb09
Pavel Demin <pavel-demin@outlook.com>
2022-12-11 11:50:49 +0100

    add common_tools and test_pps

---
+++ projects/test_pps/block_design.tcl
@@ -0,0 +1,99 @@
+# Create clk_wiz
+cell xilinx.com:ip:clk_wiz pll_0 {
+  PRIMITIVE PLL
+  PRIM_IN_FREQ.VALUE_SRC USER
+  PRIM_IN_FREQ 100.0
+  CLKOUT1_USED true
+  CLKOUT1_REQUESTED_OUT_FREQ 100.0
+  JITTER_SEL Min_O_Jitter
+  JITTER_OPTIONS PS
+  CLKIN1_UI_JITTER 600
+  USE_RESET false
+} {
+  clk_in1 adc_dco_i
+}
+
+# Create processing_system7
+cell xilinx.com:ip:processing_system7 ps_0 {
+  PCW_IMPORT_BOARD_PRESET cfg/eclypse_z7.xml
+  PCW_USE_M_AXI_GP1 1
+} {
+  M_AXI_GP0_ACLK pll_0/clk_out1
+  M_AXI_GP1_ACLK ps_0/FCLK_CLK0
+}
+
+make_bd_intf_pins_external [get_bd_intf_pins ps_0/IIC_0]
+
+# Create axi_gpio
+cell xilinx.com:ip:axi_gpio gpio_0 {
+  C_GPIO_WIDTH 5
+}
+
+make_bd_intf_pins_external [get_bd_intf_pins gpio_0/GPIO]
+
+# Create all required interconnections
+apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {
+  make_external {FIXED_IO, DDR}
+  Master Disable
+  Slave Disable
+} [get_bd_cells ps_0]
+
+# Create xlconstant
+cell xilinx.com:ip:xlconstant const_0
+
+# Create proc_sys_reset
+cell xilinx.com:ip:proc_sys_reset rst_0 {} {
+  ext_reset_in const_0/dout
+}
+
+# ADC SPI
+
+# Create axi_axis_writer
+cell pavel-demin:user:axi_axis_writer writer_0 {
+  AXI_DATA_WIDTH 32
+} {
+  aclk pll_0/clk_out1
+  aresetn rst_0/peripheral_aresetn
+}
+
+# Create axis_data_fifo
+cell xilinx.com:ip:axis_data_fifo fifo_0 {
+  TDATA_NUM_BYTES.VALUE_SRC USER
+  TDATA_NUM_BYTES 4
+  FIFO_DEPTH 1024
+  HAS_WR_DATA_COUNT true
+} {
+  S_AXIS writer_0/M_AXIS
+  s_axis_aclk pll_0/clk_out1
+  s_axis_aresetn rst_0/peripheral_aresetn
+}
+
+# Create axis_spi
+cell pavel-demin:user:axis_spi spi_0 {
+  SPI_DATA_WIDTH 24
+} {
+  S_AXIS fifo_0/M_AXIS
+  spi_data adc_spi_o
+  aclk pll_0/clk_out1
+  aresetn rst_0/peripheral_aresetn
+}
+
+# ADC
+
+# Create axis_zmod_dac
+cell pavel-demin:user:axis_zmod_adc adc_0 {
+  ADC_DATA_WIDTH 14
+} {
+  aclk pll_0/clk_out1
+  adc_data adc_data_i
+}
+
+# PPS and level measurement
+
+module common_0 {
+  source projects/common_tools/block_design.tcl
+}
+
+addr 0x40000000 4K writer_0/S_AXI /ps_0/M_AXI_GP0
+
+addr 0x80000000 4K gpio_0/S_AXI /ps_0/M_AXI_GP1
